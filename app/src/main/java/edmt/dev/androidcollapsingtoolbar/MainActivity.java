a29f31e80547edbbc5efc70e2dc6cf839a375127
Hoang Linh Nguyen Tran <eddydn@gmail.com>
2017-07-11 07:06:15 +0700

    First Commit
    First Commit

---
+++ app/src/main/java/edmt/dev/androidcollapsingtoolbar/MainActivity.java
@@ -0,0 +1,19 @@
+package edmt.dev.androidcollapsingtoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
